8fa359553d9feab8fe5f18547f7688c5363186c0
AnishAdkikar <anishadkikar1411@gmail.com>
2024-02-12 19:33:01 +0530

    initial server created

--- sample/main.go
+++ sample/main.go
@@ -1,119 +1,137 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"log"
-	"os"
-	"os/exec"
+	"net/http"
 	"strconv"
-	"strings"
+	"sync"
 
 	hnsw "github.com/AnishAdkikar/og"
 )
 
+var (
+	userHnswMap = make(map[string]*hnsw.Hnsw)
+	mu          sync.Mutex 
+)
+
+func handleNewConnection(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error parsing form data: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	userID := r.FormValue("userID")
+	M := r.FormValue("M")
+	efConstruction := r.FormValue("efConstruction")
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	h, ok := userHnswMap[userID]
+	if !ok {
+		M_int, _ := strconv.Atoi(M)
+		efConstruction_int, _ := strconv.Atoi(efConstruction)
+		h = hnsw.New(M_int, efConstruction_int)
+		userHnswMap[userID] = h
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Data received successfully")
+}
+
+func handleAddData(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var requestData struct {
+		UserID string               `json:"userId"`
+		Data   map[string][]float32 `json:"data"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error decoding JSON data: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	userID := requestData.UserID
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	h, ok := userHnswMap[userID]
+	if !ok {
+		http.Error(w, "HNSW instance not found for the user", http.StatusBadRequest)
+		return
+	}
+
+	for text, vector := range requestData.Data {
+		h.Add(vector, uint32(h.Size), text)
+		h.Size++
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Data added successfully")
+}
+
+func handleSearch(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST requests are allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var requestData struct {
+		UserID string    `json:"userId"`
+		Ef     int       `json:"ef"`
+		K      int       `json:"K"`
+		Data   []float32 `json:"data"`
+	}
+
+	err := json.NewDecoder(r.Body).Decode(&requestData)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error decoding JSON data: %s", err), http.StatusBadRequest)
+		return
+	}
+
+	userID := requestData.UserID
+	data := requestData.Data
+
+	mu.Lock()
+	defer mu.Unlock()
+
+	h, ok := userHnswMap[userID]
+	if !ok {
+		http.Error(w, "HNSW instance not found for the user", http.StatusBadRequest)
+		return
+	}
+
+	res := h.Search(data, requestData.Ef, requestData.K)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Data added successfully")
+	w.Header().Set("Content-Type", "application/json")
+
+	err = json.NewEncoder(w).Encode(res)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error encoding JSON response: %s", err), http.StatusInternalServerError)
+		return
+	}
+}
 func main() {
-	pythonScript := "../scripts/embeddings.py"
-	inputCSVFile := "../scripts/data.csv"
-	outputEmbeddingsFile := "../scripts/embeddings.txt"
 
-	if err := runPythonScript(pythonScript, inputCSVFile, outputEmbeddingsFile); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("Python file execution complete")
-	const (
-		M              = 3
-		efConstruction = 4
-		efSearch       = 2
-		K              = 1
-	)
+	http.HandleFunc("/connection", handleNewConnection)
+	http.HandleFunc("/add-data", handleAddData)
+	http.HandleFunc("/search", handleSearch)
 
-	h := hnsw.New(M, efConstruction)
-	if err := readEmbeddingsFiles(outputEmbeddingsFile, "../scripts/text_data.txt", h.Add); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println("hnsw addition execution complete")
-
-	cmd := exec.Command("python", "../scripts/search.py", "tiered bad working tired")
-	output, err := cmd.Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	var resultArray []float32
-	if err := json.Unmarshal(output, &resultArray); err != nil {
-		log.Fatal(err)
-	}
-	query := resultArray
-	fmt.Printf("Now searching with HNSW...\n")
-	result := h.Search(query, efSearch, K)
-	fmt.Println(result)
+	fmt.Println("Server listening on :8080")
+	http.ListenAndServe(":8080", nil)
 }
-
-func runPythonScript(pythonScript, inputCSVFile, outputEmbeddingsFile string) error {
-	cmd := exec.Command("python", pythonScript, inputCSVFile, outputEmbeddingsFile)
-
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err := cmd.Run()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-func readEmbeddingsFiles(dataFilePath string, stringFilePath string, addFunc func(hnsw.Point, uint32, string)) error {
-	dataFile, err := os.Open(dataFilePath)
-	if err != nil {
-		return err
-	}
-	defer dataFile.Close()
-
-	stringFile, err := os.Open(stringFilePath)
-	if err != nil {
-		return err
-	}
-	defer stringFile.Close()
-
-	scannerData := bufio.NewScanner(dataFile)
-	scannerString := bufio.NewScanner(stringFile)
-
-	lineNumber := 1 
-	for scannerData.Scan() {
-		dataLine := scannerData.Text()
-		dataValues := strings.Fields(dataLine)
-		var data []float32
-		for _, valStr := range dataValues {
-			val, err := strconv.ParseFloat(valStr, 32)
-			if err != nil {
-				return err
-			}
-			data = append(data, float32(val))
-		}
-		point := hnsw.Point(data)
-
-		if scannerString.Scan() {
-			stringValue := scannerString.Text()
-
-			addFunc(point, uint32(lineNumber), stringValue)
-
-			lineNumber++
-		} else {
-			return errors.New("second file has fewer lines than the first file")
-		}
-	}
-
-	if err := scannerData.Err(); err != nil {
-		return err
-	}
-
-	if err := scannerString.Err(); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-
