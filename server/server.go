df0c38195663ad7efeda4efc53ade230d1ada1b1
AnishAdkikar <anishadkikar1411@gmail.com>
2024-02-13 12:58:34 +0530

    lockfree hashmap bug fixes

--- server/server.go
+++ server/server.go
@@ -5,15 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
-	// "sync"
-	"github.com/dustinxie/lockfree"
+
 	hnsw "github.com/AnishAdkikar/og"
+	"github.com/dustinxie/lockfree"
 )
 
 var (
-	// userHnswMap = make(map[string]*hnsw.Hnsw)
 	userHnswMap = lockfree.NewHashMap()
-	// mu          sync.Mutex
 )
 
 func handleConnection(w http.ResponseWriter, r *http.Request) {
@@ -36,25 +34,14 @@ func handleConnection(w http.ResponseWriter, r *http.Request) {
 	userID := requestData.UserID
 	M := requestData.M
 	efConstruction := requestData.EfConstruction
-	// fmt.Println(userID, M, efConstruction)
-	// mu.Lock()
-	// defer mu.Unlock()
 
-	// h, ok := userHnswMap[userID]
-	// if !ok {
-	// 	fmt.Println("Creating new entry")
-	// 	M_int, _ := strconv.Atoi(M)
-	// 	efConstruction_int, _ := strconv.Atoi(efConstruction)
-	// 	h = hnsw.New(M_int, efConstruction_int)
-	// 	userHnswMap[userID] = h
-	// }
-	h,ok := userHnswMap.Get(userID)
+	h, ok := userHnswMap.Get(userID)
 	if !ok {
 		fmt.Println("Creating new entry")
 		M_int, _ := strconv.Atoi(M)
 		efConstruction_int, _ := strconv.Atoi(efConstruction)
 		h = hnsw.New(M_int, efConstruction_int)
-		userHnswMap.Set(userID,h)
+		userHnswMap.Set(userID, h)
 	}
 
 	w.WriteHeader(http.StatusOK)
@@ -80,20 +67,17 @@ func handleAddData(w http.ResponseWriter, r *http.Request) {
 
 	userID := requestData.UserID
 
-	// mu.Lock()
-	// defer mu.Unlock()
-    hInterface, ok := userHnswMap.Get(userID)
-    if !ok {
-        http.Error(w, "HNSW instance not found for the user", http.StatusBadRequest)
-        return
-    }
-
-    h, ok := hInterface.(*hnsw.Hnsw)
+	hInterface, ok := userHnswMap.Get(userID)
 	if !ok {
 		http.Error(w, "HNSW instance not found for the user", http.StatusBadRequest)
 		return
 	}
-	
+
+	h, ok := hInterface.(*hnsw.Hnsw)
+	if !ok {
+		http.Error(w, "HNSW instance not found for the user", http.StatusBadRequest)
+		return
+	}
 
 	for text, vector := range requestData.Data {
 		h.Add(vector, uint32(h.Size), text)
@@ -127,16 +111,13 @@ func handleSearch(w http.ResponseWriter, r *http.Request) {
 	userID := requestData.UserID
 	data := requestData.Data
 
-	// mu.Lock()
-	// defer mu.Unlock()
+	hInterface, ok := userHnswMap.Get(userID)
+	if !ok {
+		http.Error(w, "HNSW instance not found for the user", http.StatusBadRequest)
+		return
+	}
 
-    hInterface, ok := userHnswMap.Get(userID)
-    if !ok {
-        http.Error(w, "HNSW instance not found for the user", http.StatusBadRequest)
-        return
-    }
-
-    h, ok := hInterface.(*hnsw.Hnsw)
+	h, ok := hInterface.(*hnsw.Hnsw)
 	if !ok {
 		http.Error(w, "HNSW instance not found for the user", http.StatusBadRequest)
 		return
@@ -145,7 +126,6 @@ func handleSearch(w http.ResponseWriter, r *http.Request) {
 	res := h.Search(data, requestData.Ef, requestData.K)
 
 	w.WriteHeader(http.StatusOK)
-	// fmt.Fprint(w, "Data added successfully")
 	w.Header().Set("Content-Type", "application/json")
 
 	err = json.NewEncoder(w).Encode(res)
